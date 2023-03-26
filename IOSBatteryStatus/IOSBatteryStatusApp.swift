ad7a7b5a2e15d97b909e116eb1fb955271e98194
yxj <564197835@qq.com>
2023-03-26 18:10:06 +0800

    Initial Commit

---
+++ IOSBatteryStatus/IOSBatteryStatusApp.swift
@@ -0,0 +1,12 @@
+// IOSBatteryStatusApp.swift
+
+import SwiftUI
+
+@main
+struct IOSBatteryStatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
